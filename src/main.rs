e0f11a297e82418d98e3cf960cf77765fe150c28
tengine8000 <torty.emmanuel@gmail.com>
2025-12-04 16:44:24 +0100

    Initial todo-list setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 3 {
+        panic!("Too less arguments passed…");
+    }
+    let action = args[1].clone();
+    let task = args[2].clone();
+
+    println!("Action = {:?}, Task = {:?}", action, task);
 }
